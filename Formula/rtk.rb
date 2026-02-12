e86e95f5ba9905e80ccb9f3ccf4cfc14ac3d2832
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-02-12 11:34:25 +0000

    rtk 0.14.0

--- Formula/rtk.rb
+++ Formula/rtk.rb
@@ -1,21 +1,21 @@
 class Rtk < Formula
   desc "Rust Token Killer - High-performance CLI proxy to minimize LLM token consumption"
-  homepage "https://github.com/rtk-ai/rtk"
-  version "0.13.1"
+  homepage "https://www.rtk-ai.app"
+  version "0.14.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rtk-ai/rtk/releases/download/v#{version}/rtk-aarch64-apple-darwin.tar.gz"
-    sha256 "68ba56b4cd2c0b21c52ba2b006d8b8b293eb0fce27d7c4f7029e83e846ca5aff"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.14.0/rtk-aarch64-apple-darwin.tar.gz"
+    sha256 "fd19ca8844e709d456ece21e1dc6dab3518711ba8c3e6662b15457a8f2027264"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rtk-ai/rtk/releases/download/v#{version}/rtk-x86_64-apple-darwin.tar.gz"
-    sha256 "8bc7ecb968b95cc581d09d6f1f3bd0122d667f85553fd913207647c755acebd6"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.14.0/rtk-x86_64-apple-darwin.tar.gz"
+    sha256 "6501dbb3135be81d061fd85eee3d8c56e39e168583a1d7626d481c83e9a85ee8"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/rtk-ai/rtk/releases/download/v#{version}/rtk-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "6e8771a47580268d0e5227d1e919bbf56c8ec0dc339dc242f652cb6d9d04ef84"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.14.0/rtk-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "bfacfc75a406214bcfd095eb0194bbfe8c31068748c1915ddd80803987d7aa48"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rtk-ai/rtk/releases/download/v#{version}/rtk-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "681d11d85af300788ed1ace5c1ccf8c584d9f935c662e6c96f0b27a69a183550"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.14.0/rtk-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "b8456c50f307e1a2251b7847694eb50e81a4941e3db08f0b232861439d4c8b93"
   end
 
   def install
@@ -36,7 +36,7 @@ class Rtk < Formula
         # Measure your token savings
         rtk gain
 
-      Full documentation: https://github.com/rtk-ai/rtk
+      Full documentation: https://www.rtk-ai.app
     EOS
   end
 
