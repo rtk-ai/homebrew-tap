b1178dafba64918b23721fbf297f3195df12101b
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-03-25 08:03:53 +0000

    rtk 0.33

--- Formula/rtk.rb
+++ Formula/rtk.rb
@@ -1,21 +1,21 @@
 class Rtk < Formula
   desc "Rust Token Killer - High-performance CLI proxy to minimize LLM token consumption"
   homepage "https://www.rtk-ai.app"
-  version "0.33.0-rc.54"
+  version "0.33"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.33.0-rc.54/rtk-aarch64-apple-darwin.tar.gz"
-    sha256 "38b3776cdac5a7266f769720dda402d35a737f66ac72f7f6cacb19e471661c3e"
+    url "https://github.com/rtk-ai/rtk/releases/download/0.33/rtk-aarch64-apple-darwin.tar.gz"
+    sha256 "ec4ccba6b6c105abcdfd4ae7ddb3b816e00b8b8234441ad7bc44a8c6190efd99"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.33.0-rc.54/rtk-x86_64-apple-darwin.tar.gz"
-    sha256 "9dcb0b5125c0e7abb1434b144743bed85c7e3770740d001713d6b066428a0878"
+    url "https://github.com/rtk-ai/rtk/releases/download/0.33/rtk-x86_64-apple-darwin.tar.gz"
+    sha256 "2e7ee0da3ed7f8fcb2a777b5ce51fcb1227ae12a3513bcf6b43e7b0f971b9eab"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.33.0-rc.54/rtk-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "5dace7d667d49a4910bec26bd53dfec4e0cd258468ba144da5349de09f97ae0f"
+    url "https://github.com/rtk-ai/rtk/releases/download/0.33/rtk-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "2cb0df2b1458176eb30d4dbe1183b59c4173ded60b65a270569567081d25fb9e"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.33.0-rc.54/rtk-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "4d6da4303a608ac77b4806ef388adea1b4fe0851cc2d850bfc7dbec250636150"
+    url "https://github.com/rtk-ai/rtk/releases/download/0.33/rtk-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "17783d344c4fe02aef68b4644bacb1b08d8f7a8f6c0b4d6a1b5697804c24ab2b"
   end
 
   def install
