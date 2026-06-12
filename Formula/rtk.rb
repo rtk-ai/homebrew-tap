a37f6e3c5fdeb749ea6eab062dfa723ab6e33a4f
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-06-12 16:58:57 +0000

    rtk 0.42.4

--- Formula/rtk.rb
+++ Formula/rtk.rb
@@ -1,21 +1,21 @@
 class Rtk < Formula
   desc "Rust Token Killer - High-performance CLI proxy to minimize LLM token consumption"
   homepage "https://www.rtk-ai.app"
-  version "0.42.3"
+  version "0.42.4"
   license "Apache 2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.3/rtk-aarch64-apple-darwin.tar.gz"
-    sha256 "d47823afb25919e4e60838c5622e88ffa6536bc0b36a34a3f928bdccac40f614"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.4/rtk-aarch64-apple-darwin.tar.gz"
+    sha256 "f223ca074a0215af002679bc1d34ca92b93e25b3e8ae16aace6e84c06e586802"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.3/rtk-x86_64-apple-darwin.tar.gz"
-    sha256 "7c72d05cfc71b7e2f20755b3754b728acecc7c0b1fbbb08757828f9e7bedd81a"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.4/rtk-x86_64-apple-darwin.tar.gz"
+    sha256 "84121316867613e61925c209607f033b2113bb0ce312c267a79d3e3e8f221e49"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.3/rtk-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "2b7fa09d06f8dbf334c55482fad2e7ce4a1f8564bc9ed1f65d9f5992db8e5527"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.4/rtk-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "cc2b91c064eb670c097c184913c8fbcb1a943d53d7fe505375e96ba0c5b6459f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.3/rtk-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "5df764a633709cb85d248258d085d24ec95faa8bca0e6835a93cd57cadc4eb9e"
+    url "https://github.com/rtk-ai/rtk/releases/download/v0.42.4/rtk-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "34975116da11e09e502501daf758143e0b22ed3a42a10eb67fb693a6270d9e36"
   end
 
   def install
